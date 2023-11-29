4460178ec5e6e5e986b850419451ebb30b51cfb9
ren <ren.ramos33@gmail.com>
2023-11-29 13:08:00 +0800

    Removed the "blogImage" field on the blogsSchema.sql

--- src/main/resources/blogsSchema.sql
+++ src/main/resources/blogsSchema.sql
@@ -1,8 +1,7 @@
 create table if not exists Blogs(
-      blogID int not null primary key,
+      blogID int not null auto_increment primary key,
       blogTitle varchar(255) not null,
       blogContent text not null,
-      blogImage varchar(255) not null,
       blogAuthor varchar(255) not null,
       datePublished date not null
 );
